676e8d5842d80b0566386708f780d96f2979b3e9
Kermit <gwenokim@gmail.com>
2018-04-25 11:18:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,11 +54,14 @@ def processRequest(req):
     yql_url = baseurl +"crossings?"+yql_query
     print(yql_url)
     q = Request(yql_url)
-    q.add_header('Authorization: Bearer ', 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaG9ydEJyZWFrcyIsInJvbGVzIjoiUk9MRV9DVVNUT01FUiIsImlzcyI6IkJyaXRhbnkgRmVycmllcyIsImlhdCI6MTUyMjc0MjEwNCwianRpIjoiMjcxYzA2ZGMtOGQ4YS00YTZmLWE1ZDYtMDRiZThlNzEyMmU4In0.RD4zhr5Ve2Vkay-_6_ZRzKxgbjnG6B1YKZS3bazS9vs')
+    q.add_header('Authorization: Bearer', 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaG9ydEJyZWFrcyIsInJvbGVzIjoiUk9MRV9DVVNUT01FUiIsImlzcyI6IkJyaXRhbnkgRmVycmllcyIsImlhdCI6MTUyMjc0MjEwNCwianRpIjoiMjcxYzA2ZGMtOGQ4YS00YTZmLWE1ZDYtMDRiZThlNzEyMmU4In0.RD4zhr5Ve2Vkay-_6_ZRzKxgbjnG6B1YKZS3bazS9vs')
     print(q)
     result = urlopen(q).read()
+    print(result)
     data = json.loads(result)
+    print(data)
     res = makeWebhookResult(data)
+    print(res)
     return res
 
 
