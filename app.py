9aa24c5fa86b7ba2cd118d36876c1f5341f12263
Kermit <gwenokim@gmail.com>
2018-04-25 11:01:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def processRequest(req):
     yql_url = baseurl +"crossings?"+yql_query
     print(yql_url)
     q = Request(yql_url)
-    q.add_header('Authorization: Bearer ', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaG9ydEJyZWFrcyIsInJvbGVzIjoiUk9MRV9DVVNUT01FUiIsImlzcyI6IkJyaXRhbnkgRmVycmllcyIsImlhdCI6MTUyMjc0MjEwNCwianRpIjoiMjcxYzA2ZGMtOGQ4YS00YTZmLWE1ZDYtMDRiZThlNzEyMmU4In0.RD4zhr5Ve2Vkay-_6_ZRzKxgbjnG6B1YKZS3bazS9vs')
+    q.add_header('Authorization: Bearer ', 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaG9ydEJyZWFrcyIsInJvbGVzIjoiUk9MRV9DVVNUT01FUiIsImlzcyI6IkJyaXRhbnkgRmVycmllcyIsImlhdCI6MTUyMjc0MjEwNCwianRpIjoiMjcxYzA2ZGMtOGQ4YS00YTZmLWE1ZDYtMDRiZThlNzEyMmU4In0.RD4zhr5Ve2Vkay-_6_ZRzKxgbjnG6B1YKZS3bazS9vs')
     print(q)
     result = urlopen(q).read()
     data = json.loads(result)
