85d769266d5188a6dceff6820261fa3274675c3b
Kermit <gwenokim@gmail.com>
2018-04-26 10:07:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -53,10 +53,11 @@ def processRequest(req):
     yql_query = makeYqlQuery(req)
     yql_url = baseurl +"crossings?"+yql_query
     print(yql_url)
-   
-    yql_url.headers['Authorization: Bearer']='eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaG9ydEJyZWFrcyIsInJvbGVzIjoiUk9MRV9DVVNUT01FUiIsImlzcyI6IkJyaXRhbnkgRmVycmllcyIsImlhdCI6MTUyMjc0MjEwNCwianRpIjoiMjcxYzA2ZGMtOGQ4YS00YTZmLWE1ZDYtMDRiZThlNzEyMmU4In0.RD4zhr5Ve2Vkay-_6_ZRzKxgbjnG6B1YKZS3bazS9vs'
-    print(ylq_url)
-    result = urlopen(yql_url).read()
+    URL = make_response(yql_url)
+    print(URL)
+    URL.headers['Authorization: Bearer']='eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaG9ydEJyZWFrcyIsInJvbGVzIjoiUk9MRV9DVVNUT01FUiIsImlzcyI6IkJyaXRhbnkgRmVycmllcyIsImlhdCI6MTUyMjc0MjEwNCwianRpIjoiMjcxYzA2ZGMtOGQ4YS00YTZmLWE1ZDYtMDRiZThlNzEyMmU4In0.RD4zhr5Ve2Vkay-_6_ZRzKxgbjnG6B1YKZS3bazS9vs'
+    print(URL)
+    result = urlopen(URL).read()
     print(result)
     data = json.loads(result)
     print(data)
