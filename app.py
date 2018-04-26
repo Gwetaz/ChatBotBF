acfa68eee8124cda42127092c60185dfc0f0a51e
Kermit <gwenokim@gmail.com>
2018-04-26 10:56:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -53,9 +53,8 @@ def processRequest(req):
     yql_query = makeYqlQuery(req)
     yql_url = baseurl +"crossings?"+yql_query
     print(yql_url)
-    URL = make_response(yql_url)
-    print(URL)
-    URL.headers['Authorization: Bearer']='eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaG9ydEJyZWFrcyIsInJvbGVzIjoiUk9MRV9DVVNUT01FUiIsImlzcyI6IkJyaXRhbnkgRmVycmllcyIsImlhdCI6MTUyMjc0MjEwNCwianRpIjoiMjcxYzA2ZGMtOGQ4YS00YTZmLWE1ZDYtMDRiZThlNzEyMmU4In0.RD4zhr5Ve2Vkay-_6_ZRzKxgbjnG6B1YKZS3bazS9vs'
+    headers = { 'Authorization: Bearer' : 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaG9ydEJyZWFrcyIsInJvbGVzIjoiUk9MRV9DVVNUT01FUiIsImlzcyI6IkJyaXRhbnkgRmVycmllcyIsImlhdCI6MTUyMjc0MjEwNCwianRpIjoiMjcxYzA2ZGMtOGQ4YS00YTZmLWE1ZDYtMDRiZThlNzEyMmU4In0.RD4zhr5Ve2Vkay-_6_ZRzKxgbjnG6B1YKZS3bazS9vs'}
+    URL = Request(yql_url,,headers)
     print(URL)
     result = urlopen(URL).read()
     print(result)
