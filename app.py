bd8545e9aa49f0318eca826af565b50ff6e2e5f8
Kermit <gwenokim@gmail.com>
2018-04-26 11:25:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -56,9 +56,10 @@ def processRequest(req):
     headers = { 'Authorization ' : 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaG9ydEJyZWFrcyIsInJvbGVzIjoiUk9MRV9DVVNUT01FUiIsImlzcyI6IkJyaXRhbnkgRmVycmllcyIsImlhdCI6MTUyMjc0MjEwNCwianRpIjoiMjcxYzA2ZGMtOGQ4YS00YTZmLWE1ZDYtMDRiZThlNzEyMmU4In0.RD4zhr5Ve2Vkay-_6_ZRzKxgbjnG6B1YKZS3bazS9vs'}
     URL = Request(yql_url,None,headers)
     print(URL)
-    result = urlopen(URL).read()
-    print(result)
-    data = json.loads(result)
+    result = urlopen(URL)
+    lu = result.read()
+    print(lu)
+    data = json.loads(lu)
     print(data)
     res = makeWebhookResult(data)
     print(res)
