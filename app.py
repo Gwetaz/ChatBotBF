b5642339c33091e0b5d3fde0715cf2cd355e62d4
Kermit <gwenokim@gmail.com>
2018-04-26 10:58:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def processRequest(req):
     yql_url = baseurl +"crossings?"+yql_query
     print(yql_url)
     headers = { 'Authorization: Bearer' : 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaG9ydEJyZWFrcyIsInJvbGVzIjoiUk9MRV9DVVNUT01FUiIsImlzcyI6IkJyaXRhbnkgRmVycmllcyIsImlhdCI6MTUyMjc0MjEwNCwianRpIjoiMjcxYzA2ZGMtOGQ4YS00YTZmLWE1ZDYtMDRiZThlNzEyMmU4In0.RD4zhr5Ve2Vkay-_6_ZRzKxgbjnG6B1YKZS3bazS9vs'}
-    URL = Request(yql_url,,headers)
+    URL = Request(yql_url,'',headers)
     print(URL)
     result = urlopen(URL).read()
     print(result)
