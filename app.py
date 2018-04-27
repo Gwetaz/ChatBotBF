50da70ea4ff1a33bea2bfd2a0e89693eec27e848
Kermit <gwenokim@gmail.com>
2018-04-27 14:29:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def makeWebhookResult(data,req):
           "destinationName": "Je réserve ",
           "uri": "brittany-ferries.fr/510?AccountNo=&ferry=ferryonly&journeyType=return&journeyTypeState=&FCONsubmission=true&frmOGroup=9&frmORoute=&frmODay=&frmOMonthYear=&frmOMonth=&frmOYear=frmOMonthYearRestore=&frmODayRestore=&frmIGroup=5&frmIRoute=&frmIDay=frmIMonthYear=frmIMonth=frmIYear=frmIMonthYearRestore=&frmIDayRestore=&submit=Je+réserve"
         }
-      },
+      }
     }
 
 
