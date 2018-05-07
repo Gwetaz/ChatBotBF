15672aae52495cf1cfc339924a214aabc62282dc
Kermit <gwenokim@gmail.com>
2018-05-07 11:26:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,6 @@ def processRequest(req):
          result = urlopen(URL)
          lu = result.read()
          data = json.loads(lu)
-         print('alolemonde2')
          res = makeWebhookResult3(data,req)
          print("apresWebhook")
          print(res)
@@ -105,11 +104,15 @@ def processRequest(req):
     
     elif req.get("queryResult").get("action") == "HorairePrecise":
          yql_query = makeYqlQuery4(req)
+         print(yql_query)
          yql_url = baseurl +"crossings?"+yql_query
+         print(yql_url)
          headers = {}
          headers['Authorization'] = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaG9ydEJyZWFrcyIsInJvbGVzIjoiUk9MRV9DVVNUT01FUiIsImlzcyI6IkJyaXRhbnkgRmVycmllcyIsImlhdCI6MTUyMjc0MjEwNCwianRpIjoiMjcxYzA2ZGMtOGQ4YS00YTZmLWE1ZDYtMDRiZThlNzEyMmU4In0.RD4zhr5Ve2Vkay-_6_ZRzKxgbjnG6B1YKZS3bazS9vs"
          URL = Request(yql_url,headers = headers)
+         print(URL)
          result = urlopen(URL)
+         print(result)
          lu = result.read()
          data = json.loads(lu)
          res = makeWebhookResult4(data,req)
